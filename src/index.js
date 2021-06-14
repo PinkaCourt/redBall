1c6f2ec38fc8f87352e9c8bd3099af0a2b54d549
Polina <ekortika@gmail.com>
2021-06-15 00:55:49 +0300

    первый отскок

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/*
+const render = () => {
+  requestAnimationFrame(render);
+  const { top } = ball.getBoundingClientRect();
+  ball.style.top = top + 10 + "px";
+};*/
+
 const container = document.createElement("div");
 container.classList.add("container");
 
@@ -8,8 +15,6 @@ ball.setAttribute("draggable", true);
 document.body.appendChild(ball);
 document.body.appendChild(container);
 
-//console.log(ball.getBoundingClientRect());
-
 const {
   top: containerTop,
   left: containerLeft,
@@ -18,92 +23,79 @@ const {
 } = container.getBoundingClientRect();
 
 const containerBottom = containerTop + containerHeight;
+const containerRight = containerLeft + containerWidth;
 
-// dragend
-// onDragOver
-container.addEventListener(`dragend`, (event) => {
-  event.preventDefault();
-  //event.stopPropagation();
-  //const activeBall = ball;
-  // const currentContainer = event.target;
-  // event.dataTransfer.dropEffect = "move";
+ball.onmousedown = function (event) {
+  moveAt(event.pageX, event.pageY);
 
-  //console.log("event.target", event.target);
-  // const isMoveable = activeBall !== currentContainer;
-  /*
-    if (!isMoveable) {
-      return;
-    }*/
-  //event.dataTransfer.effectAllowed = "move";
-
-  //event.target.classList.remove(`selected`);
-});
-//const dragSrcEl = null;
-function handleDragStart(e) {
-  //console.log(this.style);
-  //dragSrcEl = this;
-  //e.dataTransfer.effectAllowed = "move";
-  //e.dataTransfer.dropEffect = "move";
-  //e.dataTransfer.setData("text/html", this.innerHTML);
-  //event.dataTransfer.effectAllowed = "move";
-  // dataTransfer.dropEffect = "move";
-  //this.style.cursor = "grab"; // this / e.target is the source node.
-}
-
-function handleDragStop(e) {
-  //this.style.cursor = ""; // this / e.target is the source node.
-}
-/*
-function handleDrop(e) {
-  if (e.stopPropagation) {
-    e.stopPropagation(); // Stops some browsers from redirecting.
+  // передвинуть мяч под координаты курсора
+  // и сдвинуть на половину ширины/высоты для центрирования
+  function moveAt(pageX, pageY) {
+    ball.style.left = pageX - ball.offsetWidth / 2 + "px";
+    ball.style.top = pageY - ball.offsetHeight / 2 + "px";
   }
 
-  if (dragSrcEl != this) {
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData("text/html");
-  }
+  ball.ondragstart = function () {
+    return false;
+  };
 
-  return false;
-}
-*/
-ball.addEventListener("dragstart", handleDragStart, false);
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
 
-container.addEventListener("dragstop", handleDragStop, false);
+    ball.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    ball.hidden = false;
 
-container.addEventListener(`dragover`, (event) => {
-  event.preventDefault();
-  //event.stopPropagation();
-  //const activeBall = ball;
-  // const currentContainer = event.target;
-  //event.dataTransfer.dropEffect = "move";
-});
-container.addEventListener("drop", (event) => {
-  event.preventDefault();
-
-  setInterval(() => {
-    const { top, left, height, width } = ball.getBoundingClientRect();
-    const bottom = top + height;
-    const right = left + width;
-
-    console.log("containerBottom", containerBottom);
-    console.log("top", top);
-
-    if (
-      bottom < containerBottom
-      // && top < containerTop
-    ) {
-      ball.style.top = top + 10 + "px";
-      ball.style.left = left + 5 + "px";
-    } else {
-      ball.style.top = top - 10 + "px";
-      ball.style.left = left + 5 + "px";
+    if (elemBelow === container) {
     }
-  }, 1000);
-});
-/*
-const render = () => {
-  requestAnimationFrame(render);
-  const { top } = ball.getBoundingClientRect();
-  ball.style.top = top + 10 + "px";
-};*/
+  }
+
+  // (3) перемещать по экрану
+  document.addEventListener("mousemove", onMouseMove);
+
+  // (4) положить мяч, удалить более ненужные обработчики событий
+  ball.onmouseup = function () {
+    /*ball.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    ball.hidden = false;
+
+    if (elemBelow === container) {
+    }
+*/
+    let horizontally = 0;
+    let vertically = 0;
+
+    setInterval(() => {
+      const { top, left, height, width } = ball.getBoundingClientRect();
+      const bottom = top + height;
+      const right = left + width;
+
+      if (horizontally === 0) {
+        if (right >= containerRight) {
+          horizontally = 1;
+        }
+        ball.style.left = left + 1 + "px";
+      } else {
+        if (left <= containerLeft) {
+          horizontally = 0;
+        }
+        ball.style.left = left - 1 + "px";
+      }
+
+      if (vertically === 0) {
+        if (bottom >= containerBottom) {
+          vertically = 1;
+        }
+        ball.style.top = top + 1 + "px";
+      } else {
+        if (top <= containerTop) {
+          vertically = 0;
+        }
+        ball.style.top = top - 1 + "px";
+      }
+    }, 1000 / 96);
+
+    document.removeEventListener("mousemove", onMouseMove);
+    ball.onmouseup = null;
+  };
+};
